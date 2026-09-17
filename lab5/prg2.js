37e3beea54ea09068284724cd6e418927b0c04e4
Archit Kumar Agarwal <architagarwal831248@gmail.com>
2026-09-17 14:52:18 +0530

    frontend

---
+++ lab5/prg2.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import path from "path";
+
+import { fileURLToPath } from "node:url";
+
+const port = 3333;
+
+const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+
+const dirname = path.dirname(filename);
+
+app.use(express.static(path.join(dirname, "frontend")));
+
+app.use((req, res) => {
+
+| res.status(404).send("Resource not found");
+
+});
+
+I
+
+app.listen(port, () => console.log(`prg2 is running at`, port));
