c7680b09e78a7c73b9f897a52b132261064255ea
Archit Kumar Agarwal <architagarwal831248@gmail.com>
2026-09-17 13:59:58 +0530

    basic routing with express

---
+++ lab5/app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+const app = express();
+app.get("/",(req,res)=> {
+    res.send("<h1> Hello Express</h1>");
+});
+
+app.get('/about',(req,res)=>{
+    res.end("WE are FSD Developer")
+})
+
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'})
+})
+
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:'user update'})
+})
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+
+
+
+app.use((req,res)=>{
+    res.status(404).send("Not found")
+})
+
+app.listen(3333,() => console.log("Server is running at 3333"));
---
+++ lab5/readme.md
@@ -0,0 +1,17 @@
+# Express
+Fast, unopinionated , minimalist web framework for node.js
+
+1. create npm project ` npm init -y`
+2. update package.json
+`type:module`
+3. install nodemon with `npm i nodemon -D`
+install express with `npm i express`
+5. update package.json
+
+``` 
+"scripts": {
+    "start": "node app.js"
+    "dev": "nodemon app.js"
+},
+```
+
